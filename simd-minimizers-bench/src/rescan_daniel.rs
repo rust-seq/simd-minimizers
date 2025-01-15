3002a45d0a9296a5054c2c9ebffca9219eaf6f99
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-01-16 00:15:22 +0100

    optimize rescan-daniel

--- simd-minimizers-bench/src/rescan_daniel.rs
+++ simd-minimizers-bench/src/rescan_daniel.rs
@@ -14,65 +14,77 @@ pub struct RescanDaniel {
 impl Minimizer for RescanDaniel {
     fn window_minimizers(&mut self, text: &[u8]) -> Vec<usize> {
         let mut minimizers = Vec::new();
-        minimizers_callback(text, self.w + self.k - 1, self.k, 0, |idx, _min| {
+        minimizers_callback::<false>(text, self.w + self.k - 1, self.k, |idx| {
             minimizers.push(idx);
         });
         minimizers
     }
 }
 
+type T = u64;
+
 /// ntHash constants.
-static LUT: [u64; 128] = {
-    let mut l = [0u64; 128];
-    l[b'A' as usize] = 0x3c8bfbb395c60474u64;
-    l[b'C' as usize] = 0x3193c18562a02b4cu64;
-    l[b'G' as usize] = 0x20323ed082572324u64;
-    l[b'T' as usize] = 0x295549f54be24456u64;
+static LUT: [T; 128] = {
+    let mut l = [0; 128];
+    l[b'A' as usize] = 0x3c8bfbb395c60474u64 as T;
+    l[b'C' as usize] = 0x3193c18562a02b4cu64 as T;
+    l[b'G' as usize] = 0x20323ed082572324u64 as T;
+    l[b'T' as usize] = 0x295549f54be24456u64 as T;
     l
 };
 
 /// Robust winnowing.
-fn minimizers_callback(s: &[u8], l: usize, k: usize, prev: u64, mut f: impl FnMut(usize, u64)) {
+pub fn minimizers_callback<const DEDUP: bool>(
+    s: &[u8],
+    l: usize,
+    k: usize,
+    mut f: impl FnMut(usize),
+) {
     let mut min = 0;
     let mut min_idx = 0;
     let mut curr = 0;
 
     for (i, win) in s.windows(l).enumerate() {
         if i == 0 || i > min_idx {
-            let (m_idx, m, c) = minimum(win, k, prev);
+            let (m_idx, m, c) = minimum(win, k);
             min_idx = i + m_idx;
             min = m;
             curr = c;
-            f(min_idx, min);
+            f(min_idx);
         } else {
             curr = curr.rotate_left(1)
                 ^ LUT[win[l - 1 - k] as usize].rotate_left(k as u32)
                 ^ LUT[win[l - 1] as usize];
-            let h = prev ^ curr;
+            let h = curr;
 
             if h < min {
                 min_idx = i + l - k;
                 min = h;
+                if DEDUP {
+                    f(min_idx);
+                }
             }
-            f(min_idx, min);
+            if !DEDUP {
+                f(min_idx);
+            }
         }
     }
 }
 
-/// Get the rightmost minimum kmer.
-fn minimum(s: &[u8], k: usize, prev: u64) -> (usize, u64, u64) {
+/// Get the leftmost minimum kmer.
+fn minimum(s: &[u8], k: usize) -> (usize, T, T) {
     let mut curr = 0;
 
     for (i, &b) in s[..k].iter().enumerate() {
         curr ^= LUT[b as usize].rotate_left((k - 1 - i) as u32);
     }
 
-    let mut min = prev ^ curr;
+    let mut min = curr;
     let mut min_idx = 0;
 
     for (i, &b) in s[k..].iter().enumerate() {
         curr = curr.rotate_left(1) ^ LUT[s[i] as usize].rotate_left(k as u32) ^ LUT[b as usize];
-        let h = prev ^ curr;
+        let h = curr;
 
         // This was changed from <= to < to ensure the leftmost minimum is returned.
         if h < min {

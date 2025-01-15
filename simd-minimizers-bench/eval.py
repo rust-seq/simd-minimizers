1a3b18925c275cbea46f40694856819a917df0c0
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-01-16 00:16:22 +0100

    Add results and eval.py to make a table out of them

---
+++ simd-minimizers-bench/eval.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import pandas as pd
+from pathlib import Path
+import tabulate
+
+# from matplotlib.ticker import LogLocator
+# from matplotlib.colors import to_rgba
+
+# Limit matplot lib print precision
+pd.set_option("display.precision", 2)
+pd.set_option("display.float_format", "{:0.2f}".format)
+
+print("table")
+DF = pd.read_json("results.json")
+
+
+DF["canonical"] = DF["name"].apply(lambda x: x.startswith("canonical "))
+DF["name"] = DF["name"].str.replace("canonical ", "")
+
+print(tabulate.tabulate(DF, headers=DF.columns, tablefmt="orgtbl", floatfmt=".2f"))
+
+# Incremental table
+df = DF[(DF["experiment"] == "incremental") & (DF.w == 11)][["name", "time"]]
+print(tabulate.tabulate(df, headers=df.columns, tablefmt="orgtbl", floatfmt=".2f"))
+print(df.to_latex(index=False, float_format="%.2f"))
+
+# External table
+df = DF[DF["experiment"] == "external"]
+df = df.pivot(index="name", columns=["w", "k", "canonical"], values="time")
+print(tabulate.tabulate(df, headers=df.columns, tablefmt="orgtbl", floatfmt=".2f"))
+print(df.to_latex(float_format="%.2f"))
+
+# # Sliding min plot
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# df = DF[DF["experiment"] == "sliding_min"]
+# ax = sns.lineplot(data=df, x="w", y="time", hue="name")
+# # log base 2
+# ax.set_xscale("log", base=2)
+# ax.set_ylim(0, 30)
+# ax.grid(axis="y")
+# plt.show()

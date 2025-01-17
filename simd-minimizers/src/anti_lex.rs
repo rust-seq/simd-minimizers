f7ffaedd98ed98bdc146982a9e50cd5388005fab
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-01-17 21:21:52 +0100

    AntiLexHash uses the sequence's bit width

--- simd-minimizers/src/anti_lex.rs
+++ simd-minimizers/src/anti_lex.rs
@@ -5,15 +5,31 @@
 //! TODO: Figure out how this works with reverse complements.
 
 use crate::nthash::Captures;
-use packed_seq::{PackedSeq, Seq, S};
+use packed_seq::{Seq, S};
+
+/// For k b-bit chars, the k*b-bit mask, and the mask for the most significant character.
+fn anti_and_mask(k: usize, b: usize) -> (u32, u32) {
+    let mask = if b * k < 32 {
+        (1 << (b * k)) - 1
+    } else {
+        u32::MAX
+    };
+    let anti = if b * k <= 32 {
+        ((1 << b) - 1) << (b * (k - 1))
+    } else {
+        ((1 << b) - 1) << (32 - b)
+    };
+    (anti, mask)
+}
 
 /// Naively compute the 32-bit anti-lex hash of a single k-mer.
 pub fn anti_lex_hash_kmer<'s>(seq: impl Seq<'s>) -> u32 {
+    let b = seq.bits_per_char();
     let k = seq.len();
     let mut hfw: u32 = 0;
-    let anti = if k <= 16 { 3 << (2 * k - 2) } else { 3 << 30 };
+    let (anti, _mask) = anti_and_mask(k, b);
     seq.iter_bp().for_each(|a| {
-        hfw = (hfw << 2) ^ a as u32;
+        hfw = (hfw << b) ^ a as u32;
     });
     hfw ^ anti
 }
@@ -24,16 +40,16 @@ pub fn anti_lex_hash_seq_scalar<'s>(
     seq: impl Seq<'s>,
     k: usize,
 ) -> impl ExactSizeIterator<Item = u32> + Captures<&'s ()> {
+    let b = seq.bits_per_char();
     assert!(k > 0);
     let mut hfw: u32 = 0;
     let mut add = seq.iter_bp();
-    let mask = if k < 16 { (1 << (2 * k)) - 1 } else { u32::MAX };
-    let anti = if k <= 16 { 3 << (2 * k - 2) } else { 3 << 30 };
+    let (anti, mask) = anti_and_mask(k, b);
     add.by_ref().take(k - 1).for_each(|a| {
-        hfw = (hfw << 2) ^ (a as u32);
+        hfw = (hfw << b) ^ (a as u32);
     });
     add.map(move |a| {
-        hfw = ((hfw << 2) ^ (a as u32)) & mask;
+        hfw = ((hfw << b) ^ (a as u32)) & mask;
         hfw ^ anti
     })
 }
@@ -42,28 +58,30 @@ pub fn anti_lex_hash_seq_scalar<'s>(
 /// The tail is returned separately.
 /// Returned chunks overlap by w-1 hashes. Set w=1 for non-overlapping chunks.
 pub fn anti_lex_hash_seq_simd<'s>(
-    seq: PackedSeq<'s>,
+    seq: impl Seq<'s>,
     k: usize,
     w: usize,
 ) -> (
     impl ExactSizeIterator<Item = S> + Captures<&'s ()>,
     impl ExactSizeIterator<Item = u32> + Captures<&'s ()>,
 ) {
+    let b = seq.bits_per_char();
     assert!(k > 0);
     assert!(w > 0);
 
     let mut h_fw = S::splat(0);
     let (mut add, tail) = seq.par_iter_bp(k + w - 1);
 
-    let mask = S::splat(if k < 16 { (1 << (2 * k)) - 1 } else { u32::MAX });
-    let anti = S::splat(if k <= 16 { 3 << (2 * k - 2) } else { 3 << 30 });
+    let (anti, mask) = anti_and_mask(k, b);
+    let anti = S::splat(anti);
+    let mask = S::splat(mask);
 
     add.by_ref().take(k - 1).for_each(|a| {
-        h_fw = (h_fw << 2) ^ a;
+        h_fw = (h_fw << b as i32) ^ a;
     });
 
     let it = add.map(move |a| {
-        h_fw = ((h_fw << 2) ^ a) & mask;
+        h_fw = ((h_fw << b as i32) ^ a) & mask;
         h_fw ^ anti
     });
 
@@ -75,17 +93,20 @@ pub fn anti_lex_hash_seq_simd<'s>(
 /// A function that 'eats' added and removed bases, and returns the updated hash.
 /// The distance between them must be k-1, and the first k-1 removed bases must be 0.
 /// The first k-1 returned values will be useless.
-pub fn anti_lex_hash_mapper(k: usize, w: usize) -> impl FnMut(S) -> S + Clone {
+pub fn anti_lex_hash_mapper<'s, Sq: Seq<'s>>(k: usize, w: usize) -> impl FnMut(S) -> S + Clone {
+    let b = Sq::BITS_PER_CHAR;
+    assert!(k > 0);
     assert!(k > 0);
     assert!(w > 0);
 
-    let mask = S::splat(if k < 16 { (1 << (2 * k)) - 1 } else { u32::MAX });
-    let anti = S::splat(if k <= 16 { 3 << (2 * k - 2) } else { 3 << 30 });
+    let (anti, mask) = anti_and_mask(k, b);
+    let anti = S::splat(anti);
+    let mask = S::splat(mask);
 
     let mut h_fw = S::splat(0);
 
     move |a| {
-        h_fw = ((h_fw << 2) ^ a) & mask;
+        h_fw = ((h_fw << b as i32) ^ a) & mask;
         h_fw ^ anti
     }
 }

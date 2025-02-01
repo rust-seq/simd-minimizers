23b5a18b51c23e4d7d849340ba8a2a1b13b04059
Igor Martayan <igor@martayan.org>
2025-02-01 18:47:19 +0100

    Add append_unique_vals_2

--- simd-minimizers/src/intrinsics/dedup.rs
+++ simd-minimizers/src/intrinsics/dedup.rs
@@ -24,6 +24,36 @@ pub unsafe fn append_unique_vals(old: S, new: S, vals: S, v: &mut [u32], write_i
 }
 
 /// Dedup adjacent `new` values (starting with the last element of `old`).
+/// If an element is different from the preceding element, append the corresponding element of `vals` to `v[write_idx]` and `vals2` to `v2[write_idx]`.
+#[inline(always)]
+#[cfg(not(any(target_feature = "avx2", target_feature = "neon")))]
+pub unsafe fn append_unique_vals_2(
+    old: S,
+    new: S,
+    vals: S,
+    vals2: S,
+    v: &mut [u32],
+    v2: &mut [u32],
+    write_idx: &mut usize,
+) {
+    unsafe {
+        let old = old.to_array();
+        let new = new.to_array();
+        let vals = vals.to_array();
+        let vals2 = vals2.to_array();
+        let mut prec = old[7];
+        for (i, &curr) in new.iter().enumerate() {
+            if curr != prec {
+                v.as_mut_ptr().add(*write_idx).write(vals[i]);
+                v2.as_mut_ptr().add(*write_idx).write(vals2[i]);
+                *write_idx += 1;
+                prec = curr;
+            }
+        }
+    }
+}
+
+/// Dedup adjacent `new` values (starting with the last element of `old`).
 /// If an element is different from the preceding element, append the corresponding element of `vals` to `v[write_idx]`.
 ///
 /// Based on Daniel Lemire's blog.
@@ -48,6 +78,46 @@ pub unsafe fn append_unique_vals(old: S, new: S, vals: S, v: &mut [u32], write_i
         let key = transmute(UNIQSHUF[m]);
         let val = _mm256_permutevar8x32_epi32(vals, key);
         _mm256_storeu_si256(v.as_mut_ptr().add(*write_idx) as *mut __m256i, val);
+        *write_idx += numberofnewvalues;
+    }
+}
+
+/// Dedup adjacent `new` values (starting with the last element of `old`).
+/// If an element is different from the preceding element, append the corresponding element of `vals` to `v[write_idx]` and `vals2` to `v2[write_idx]`.
+///
+/// Based on Daniel Lemire's blog.
+/// <https://lemire.me/blog/2017/04/10/removing-duplicates-from-lists-quickly/>
+/// <https://github.com/lemire/Code-used-on-Daniel-Lemire-s-blog/blob/edfd0e8b809d9a57527a7990c4bb44b9d1d05a69/2017/04/10/removeduplicates.cpp>
+#[cfg(target_feature = "avx2")]
+#[inline(always)]
+pub unsafe fn append_unique_vals_2(
+    old: S,
+    new: S,
+    vals: S,
+    vals2: S,
+    v: &mut [u32],
+    v2: &mut [u32],
+    write_idx: &mut usize,
+) {
+    unsafe {
+        use core::arch::x86_64::*;
+
+        let old = transmute(old);
+        let new = transmute(new);
+        let vals = transmute(vals);
+        let vals2 = transmute(vals2);
+
+        let recon = _mm256_blend_epi32(old, new, 0b01111111);
+        let movebyone_mask = _mm256_set_epi32(6, 5, 4, 3, 2, 1, 0, 7); // rotate shuffle
+        let vec_tmp = _mm256_permutevar8x32_epi32(recon, movebyone_mask);
+
+        let m = _mm256_movemask_ps(transmute(_mm256_cmpeq_epi32(vec_tmp, new))) as usize;
+        let numberofnewvalues = L - m.count_ones() as usize;
+        let key = transmute(UNIQSHUF[m]);
+        let val = _mm256_permutevar8x32_epi32(vals, key);
+        _mm256_storeu_si256(v.as_mut_ptr().add(*write_idx) as *mut __m256i, val);
+        let val2 = _mm256_permutevar8x32_epi32(vals2, key);
+        _mm256_storeu_si256(v2.as_mut_ptr().add(*write_idx) as *mut __m256i, val2);
         *write_idx += numberofnewvalues;
     }
 }
@@ -102,6 +172,73 @@ pub unsafe fn append_unique_vals(old: S, new: S, vals: S, v: &mut [u32], write_i
         let r2 = vqtbl2q_u8(t, i2);
         let val: S = transmute((r1, r2));
         vst1_u32_x4(v.as_mut_ptr().add(*write_idx), transmute(val));
+        *write_idx += numberofnewvalues;
+    }
+}
+
+/// Dedup adjacent `new` values (starting with the last element of `old`).
+/// If an element is different from the preceding element, append the corresponding element of `vals` to `v[write_idx]` and `vals2` to `v2[write_idx]`.
+///
+/// Somewhat based on Daniel Lemire's blog.
+/// <https://lemire.me/blog/2017/04/10/removing-duplicates-from-lists-quickly/>
+/// <https://github.com/lemire/Code-used-on-Daniel-Lemire-s-blog/blob/edfd0e8b809d9a57527a7990c4bb44b9d1d05a69/2017/04/10/removeduplicates.cpp>
+#[inline(always)]
+#[cfg(target_feature = "neon")]
+pub unsafe fn append_unique_vals_2(
+    old: S,
+    new: S,
+    vals: S,
+    vals2: S,
+    v: &mut [u32],
+    v2: &mut [u32],
+    write_idx: &mut usize,
+) {
+    unsafe {
+        use core::arch::aarch64::{vpaddd_u64, vpaddlq_u32, vqtbl2q_u8, vst1_u32_x4};
+        use wide::u32x4;
+
+        let new_old_mask = S::new([
+            u32::MAX,
+            u32::MAX,
+            u32::MAX,
+            u32::MAX,
+            u32::MAX,
+            u32::MAX,
+            u32::MAX,
+            0,
+        ]);
+        let recon = new_old_mask.blend(new, old);
+
+        let rotate_idx = S::new([7, 0, 1, 2, 3, 4, 5, 6]);
+        let idx = rotate_idx * S::splat(0x04_04_04_04) + S::splat(0x03_02_01_00);
+        let (i1, i2) = transmute(idx);
+        let t = transmute(recon);
+        let r1 = vqtbl2q_u8(t, i1);
+        let r2 = vqtbl2q_u8(t, i2);
+        let prec: S = transmute((r1, r2));
+
+        let dup = prec.cmp_eq(new);
+        let (d1, d2): (u32x4, u32x4) = transmute(dup);
+        let pow1 = u32x4::new([1, 2, 4, 8]);
+        let pow2 = u32x4::new([16, 32, 64, 128]);
+        let m1 = vpaddd_u64(vpaddlq_u32(transmute(d1 & pow1)));
+        let m2 = vpaddd_u64(vpaddlq_u32(transmute(d2 & pow2)));
+        let m = (m1 | m2) as usize;
+
+        let numberofnewvalues = L - m.count_ones() as usize;
+        let key = UNIQSHUF[m];
+        let idx = key * S::splat(0x04_04_04_04) + S::splat(0x03_02_01_00);
+        let (i1, i2) = transmute(idx);
+        let t = transmute(vals);
+        let r1 = vqtbl2q_u8(t, i1);
+        let r2 = vqtbl2q_u8(t, i2);
+        let val: S = transmute((r1, r2));
+        vst1_u32_x4(v.as_mut_ptr().add(*write_idx), transmute(val));
+        let t = transmute(vals2);
+        let r1 = vqtbl2q_u8(t, i1);
+        let r2 = vqtbl2q_u8(t, i2);
+        let val2: S = transmute((r1, r2));
+        vst1_u32_x4(v2.as_mut_ptr().add(*write_idx), transmute(val2));
         *write_idx += numberofnewvalues;
     }
 }

c2a9b30b193e0898433ab37967dbb82e2dbeb956
Igor Martayan <igor@martayan.org>
2025-06-28 12:16:31 +0200

    Simplify the NEON version of append_unique_vals

--- simd-minimizers/src/intrinsics/dedup.rs
+++ simd-minimizers/src/intrinsics/dedup.rs
@@ -132,7 +132,7 @@ pub unsafe fn append_unique_vals_2(
 #[cfg(target_feature = "neon")]
 pub unsafe fn append_unique_vals(old: S, new: S, vals: S, v: &mut [u32], write_idx: &mut usize) {
     unsafe {
-        use core::arch::aarch64::{vpaddd_u64, vpaddlq_u32, vqtbl2q_u8, vst1_u32_x4};
+        use core::arch::aarch64::{vaddvq_u32, vqtbl2q_u8, vst1_u32_x4};
         use wide::u32x4;
 
         let new_old_mask = S::new([
@@ -147,9 +147,19 @@ pub unsafe fn append_unique_vals(old: S, new: S, vals: S, v: &mut [u32], write_i
         ]);
         let recon = new_old_mask.blend(new, old);
 
-        let rotate_idx = S::new([7, 0, 1, 2, 3, 4, 5, 6]);
-        let idx = rotate_idx * S::splat(0x04_04_04_04) + S::splat(0x03_02_01_00);
-        let (i1, i2) = transmute(idx);
+        // let rotate_idx = S::new([7, 0, 1, 2, 3, 4, 5, 6]);
+        // let idx = rotate_idx * S::splat(0x04_04_04_04) + S::splat(0x03_02_01_00);
+        // let (i1, i2) = transmute(idx);
+        let (i1, i2) = transmute([
+            0x1F1E1D1Cu32,
+            0x03020100,
+            0x07060504,
+            0x0B0A0908,
+            0x0F0E0D0C,
+            0x13121110,
+            0x17161514,
+            0x1B1A1918,
+        ]);
         let t = transmute(recon);
         let r1 = vqtbl2q_u8(t, i1);
         let r2 = vqtbl2q_u8(t, i2);
@@ -159,8 +169,8 @@ pub unsafe fn append_unique_vals(old: S, new: S, vals: S, v: &mut [u32], write_i
         let (d1, d2): (u32x4, u32x4) = transmute(dup);
         let pow1 = u32x4::new([1, 2, 4, 8]);
         let pow2 = u32x4::new([16, 32, 64, 128]);
-        let m1 = vpaddd_u64(vpaddlq_u32(transmute(d1 & pow1)));
-        let m2 = vpaddd_u64(vpaddlq_u32(transmute(d2 & pow2)));
+        let m1 = vaddvq_u32(transmute(d1 & pow1));
+        let m2 = vaddvq_u32(transmute(d2 & pow2));
         let m = (m1 | m2) as usize;
 
         let numberofnewvalues = L - m.count_ones() as usize;
@@ -194,7 +204,7 @@ pub unsafe fn append_unique_vals_2(
     write_idx: &mut usize,
 ) {
     unsafe {
-        use core::arch::aarch64::{vpaddd_u64, vpaddlq_u32, vqtbl2q_u8, vst1_u32_x4};
+        use core::arch::aarch64::{vaddvq_u32, vqtbl2q_u8, vst1_u32_x4};
         use wide::u32x4;
 
         let new_old_mask = S::new([
@@ -209,9 +219,19 @@ pub unsafe fn append_unique_vals_2(
         ]);
         let recon = new_old_mask.blend(new, old);
 
-        let rotate_idx = S::new([7, 0, 1, 2, 3, 4, 5, 6]);
-        let idx = rotate_idx * S::splat(0x04_04_04_04) + S::splat(0x03_02_01_00);
-        let (i1, i2) = transmute(idx);
+        // let rotate_idx = S::new([7, 0, 1, 2, 3, 4, 5, 6]);
+        // let idx = rotate_idx * S::splat(0x04_04_04_04) + S::splat(0x03_02_01_00);
+        // let (i1, i2) = transmute(idx);
+        let (i1, i2) = transmute([
+            0x1F1E1D1Cu32,
+            0x03020100,
+            0x07060504,
+            0x0B0A0908,
+            0x0F0E0D0C,
+            0x13121110,
+            0x17161514,
+            0x1B1A1918,
+        ]);
         let t = transmute(recon);
         let r1 = vqtbl2q_u8(t, i1);
         let r2 = vqtbl2q_u8(t, i2);
@@ -221,8 +241,8 @@ pub unsafe fn append_unique_vals_2(
         let (d1, d2): (u32x4, u32x4) = transmute(dup);
         let pow1 = u32x4::new([1, 2, 4, 8]);
         let pow2 = u32x4::new([16, 32, 64, 128]);
-        let m1 = vpaddd_u64(vpaddlq_u32(transmute(d1 & pow1)));
-        let m2 = vpaddd_u64(vpaddlq_u32(transmute(d2 & pow2)));
+        let m1 = vaddvq_u32(transmute(d1 & pow1));
+        let m2 = vaddvq_u32(transmute(d2 & pow2));
         let m = (m1 | m2) as usize;
 
         let numberofnewvalues = L - m.count_ones() as usize;
